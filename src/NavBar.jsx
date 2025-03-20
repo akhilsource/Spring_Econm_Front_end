8bb1e9555dec6d97e5acb7c80e7521631196951a
akhilsource <saiakhil14884@gmail.com>
2025-03-20 15:29:59 +0530

    Env vars adjusted2

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Navbar.css';
+import './NavBar.css';
 
 function NavBar() {
     const navigate = useNavigate();
